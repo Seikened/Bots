edb0367011733ede180f343208da4281936b228b
Seikened <fernanlee2131@gmail.com>
2021-10-08 23:00:04 -0500

    Update viernes.py

--- viernes.py
+++ viernes.py
@@ -1,13 +1,18 @@
 import telebot
+import os
 
 bot = telebot.TeleBot("2001341521:AAF9OlA1_ZykxIR_JgX__T6zy2G18CieFj8")
 
 @bot.message_handler(commands=['start', 'help','diego'])
 def send_welcome(message):
-	bot.reply_to(message, "Que onda diegoooon!!!!!▲?")
+	bot.reply_to(message, "QVe el ejemplo xdxdxd!!!!!▲?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
+
+
+
 bot.infinity_polling()
+
